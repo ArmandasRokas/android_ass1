1e95ab04428ffae2c71b9b2b6ca6fc511664224c
Armandas <armandas.rokas@gmail.com>
2019-09-15 16:48:02 +0200

    Added two buttons, TextView and EditText

--- app/src/main/java/dk/rokas/ass1/MainActivity.java
+++ app/src/main/java/dk/rokas/ass1/MainActivity.java
@@ -3,12 +3,41 @@ package dk.rokas.ass1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private TextView tv;
+    private Button b1;
+    private Button b2;
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textView);
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+        et = findViewById(R.id.editText);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("You pushed the wrong button");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Der blev trykket");
+                String input = et.getText().toString();
+                tv.setText(String.format("Velkommen %s",input));
+            }
+        });
     }
 }
