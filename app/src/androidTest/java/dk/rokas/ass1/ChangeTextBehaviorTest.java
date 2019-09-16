975651624f6824bfd07b2de9554d2223393cda2f
Armandas <armandas.rokas@gmail.com>
2019-09-16 09:37:36 +0200

    implemented test til UI

---
+++ app/src/androidTest/java/dk/rokas/ass1/ChangeTextBehaviorTest.java
@@ -0,0 +1,50 @@
+package dk.rokas.ass1;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ChangeTextBehaviorTest {
+
+    private String stringToBetyped;
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.editText))
+                .perform(replaceText(stringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.button2)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.textView))
+                .check(matches(withText(stringToBetyped)));
+    }
+}
